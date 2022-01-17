f0ead5d865634142c0ed8380c119eda5f1dda5ee
Vxcncls <leo_amaral21@hotmail.com>
2022-01-17 00:16:14 -0300

    scrollReveal fixed

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
+  #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
